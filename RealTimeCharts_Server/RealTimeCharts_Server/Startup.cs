f10a2c94eb7dbd8113a051f3f05e28d6f847fb6e
jke94 <jcarracedo.dev@gmail.com>
2021-08-10 17:53:37 +0200

    Add more configurations.

--- RealTimeCharts_Server/RealTimeCharts_Server/Startup.cs
+++ RealTimeCharts_Server/RealTimeCharts_Server/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using RealTimeCharts_Server.HubConfigs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,6 +35,8 @@ namespace RealTimeCharts_Server
                 .AllowAnyHeader()
                 .AllowCredentials());
             });
+
+            services.AddSignalR();
             services.AddControllers();
         }
 
@@ -56,6 +59,7 @@ namespace RealTimeCharts_Server
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<ChartHub>("/chart");
             });
         }
     }
