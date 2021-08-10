ba999627cede0bd31ea160f979805077dd4e57dd
jke94 <jcarracedo.dev@gmail.com>
2021-08-10 20:40:40 +0200

    Last client adjustments.

--- RealTimeCharts-Client/src/app/app.module.ts
+++ RealTimeCharts-Client/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChartsModule } from 'ng2-charts';
+import { HttpClientModule } from '@angular/common/http';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarChartComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    ChartsModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
