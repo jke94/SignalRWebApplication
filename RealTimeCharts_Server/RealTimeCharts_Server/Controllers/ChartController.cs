791dc6e2520487a1dd384b8f804d2e6c732042e4
jke94 <jcarracedo.dev@gmail.com>
2021-08-11 20:04:02 +0200

    Refactor server and add new client for POST data.

--- RealTimeCharts_Server/RealTimeCharts_Server/Controllers/ChartController.cs
+++ RealTimeCharts_Server/RealTimeCharts_Server/Controllers/ChartController.cs
@@ -1,26 +1,64 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SignalR;
-using RealTimeCharts_Server.HubConfigs;
-using RealTimeCharts_Server.TimerFeatures;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace RealTimeCharts_Server.Controllers
+﻿namespace RealTimeCharts_Server.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.SignalR;
+    using Microsoft.Extensions.Logging;
+    using RealTimeCharts_Server.HubConfigs;
+    using RealTimeCharts_Server.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
     [Route("api/[controller]")]
     [ApiController]
     public class ChartController : ControllerBase
     {
         private IHubContext<ChartHub> _hub;
-        public ChartController(IHubContext<ChartHub> hub)
+        private readonly ILogger _logger;
+        public ChartController( IHubContext<ChartHub> hub, 
+                                ILogger<ChartController> logger
+            )
         {
+            _logger = logger;
             _hub = hub;
         }
-        public IActionResult Get()
+        [HttpGet]
+        public async Task<IActionResult> Get()
         {
-            var timerManager = new TimerManager(() => _hub.Clients.All.SendAsync("transferchartdata", DataManager.GetData()));
+            var r = new Random();
+            var data = new List<ChartModel>()
+                {
+                    new ChartModel { Data = new List<int> { r.Next(1, 100) }, Label = "Data1" },
+                    new ChartModel { Data = new List<int> { r.Next(1, 100) }, Label = "Data2" },
+                    new ChartModel { Data = new List<int> { r.Next(1, 100) }, Label = "Data3" },
+                    new ChartModel { Data = new List<int> { r.Next(1, 100) }, Label = "Data4" },
+                    new ChartModel { Data = new List<int> { r.Next(1, 100) }, Label = "Data5" },
+                    new ChartModel { Data = new List<int> { r.Next(1, 100) }, Label = "Data6" },
+                    new ChartModel { Data = new List<int> { r.Next(1, 100) }, Label = "Data7" }
+                };
+
+            Task<List<ChartModel>> myTask = Task.Run(async () => 
+            {
+                // Simulate procesing in the server.
+                await Task.Delay(r.Next(1, 2000));
+                return data; 
+            });
+           
+            var result = await Task.FromResult(myTask.Result);
+            
+            data.ForEach(element => _logger.LogInformation(
+                string.Concat(element.Label, ": Value = ", element.Data[0].ToString())));
+            
+            return Ok(new { Message = "Request GET Completed", data });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddMetric(List<ChartModel> chartModelList)
+        {
+            await _hub.Clients.All.SendAsync("transferchartdata", chartModelList);
+
+            chartModelList.ForEach(element => _logger.LogInformation(
+                string.Concat(element.Label, ": Value = ", element.Data[0].ToString())));
             return Ok(new { Message = "Request Completed" });
         }
     }
