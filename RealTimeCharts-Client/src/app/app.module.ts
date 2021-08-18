a9c2f41926dc2fed6c92345f7936e23b481fdaa5
jke94 <jcarracedo.dev@gmail.com>
2021-08-18 15:52:05 +0200

    Creation by default charts.

--- RealTimeCharts-Client/src/app/app.module.ts
+++ RealTimeCharts-Client/src/app/app.module.ts
@@ -7,12 +7,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { BarChartComponent } from './components/bar-chart/bar-chart.component';
 import { BubbleAreaChartComponent } from './components/bubble-area-chart/bubble-area-chart.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { DoughnutChartComponent } from './components/doughnut-chart/doughnut-chart/doughnut-chart.component';
+import { LineChartComponent } from './components/line-chart/line-chart.component';
+import { PieChartComponent } from './components/pie-chart/pie-chart.component';
+import { ScatterAreaChartComponent } from './components/scatter-area-chart/scatter-area-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BarChartComponent,
     BubbleAreaChartComponent,
+    DoughnutChartComponent,
+    LineChartComponent,
+    PieChartComponent,
+    ScatterAreaChartComponent,
   ],
   imports: [
     BrowserModule,
